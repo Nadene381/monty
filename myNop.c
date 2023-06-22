6ce52d5c7933b303296e76353ef1e5acd0fbb142
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-22 14:10:27 -0700

    Stacks & Queues

---
+++ myNop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - Doesn't do anything/ NULL statement
+ * @stack: Double pointer passed in function
+ * @line_number: variable passed in function
+ * Return: 0
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+stack = stack;
+line_number = line_number;
+}
