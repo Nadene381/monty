577087cee38a33b45898eb6a839ed84e225b34dd
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-22 14:12:33 -0700

    Stacks & Queues

--- monty.h
+++ monty.h
@@ -1,7 +1,7 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-**
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
