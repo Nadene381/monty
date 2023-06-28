347526d5031e6ad940e9ffcdd9a2041002bd5e32
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-28 10:09:22 -0700

    monty

--- monty.h
+++ monty.h
@@ -37,5 +37,6 @@ void (*f)(stack_t **stack, unsigned int line_number);
 void mySwitch(const char *opcode, const char *par, unsigned int line_number);
 void push(stack_t **stack, int n, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 #endif
